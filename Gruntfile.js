523116adb4289022fe4bfc64947e74e4f95c8362
Ben Alman <cowboy@rj3.net>
2015-06-16 13:41:38 -0400

    Example project build process: improved readme generation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+var path = require('path');
+var yaml = require('js-yaml');
+
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-markdown');
   grunt.loadNpmTasks('grunt-contrib-clean');
@@ -5,11 +8,77 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   var githubUrl = 'https://github.com/bocoup/deployment-workflow/';
+  var branch = 'twenty';
+
+  // Handle <!--foo bar baz--> "templates"
+  var processTemplates = function(methods) {
+    return [/<!--\s*(\S+)\s*(.*?)\s*-->/g, function(src, all, method, args) {
+      var fn = methods[method] || function() { return all; };
+      return fn.apply(null, [src].concat(args.split(' ')));
+    }];
+  };
+
+  // Replacements to make before the markdown is parsed into HTML.
+  var preReplacements = [
+    processTemplates({
+      // Display specified role task/template files.
+      'role-files': function(src, role) {
+        var glob, files;
+        var rolebase = 'ansible/roles/' + role;
+        var tasks = [];
+        glob = rolebase + '/tasks/*.yml';
+        files = grunt.file.expand({cwd: 'deploy'}, glob);
+        files.forEach(function(filepath) {
+          var filename = path.basename(filepath);
+          var lines = grunt.file.read('deploy/' + filepath).split('\n');
+          var memo = {};
+          var i = lines.length - 1;
+          var doc;
+          while (i--) {
+            try {
+              doc = yaml.safeLoad(lines.slice(i).join('\n'));
+            } catch (err) {}
+            var name = doc && doc[0] && doc[0].name;
+            if (name && !memo[name]) {
+              memo[name] = true;
+              tasks.unshift(' * [' + name + '](' + filepath + '#L' + (i + 1) + ')');
+            }
+          }
+          tasks.unshift('* [' + filename + '](' + filepath + ')');
+        });
+        var templates = [];
+        glob = rolebase + '/templates/*';
+        files = grunt.file.expand({cwd: 'deploy'}, glob);
+        files.forEach(function(filepath) {
+          var filename = path.basename(filepath);
+          templates.push('* [' + filename + '](' + filepath + ')');
+        });
+        var output = [
+          'This role contains the following files and tasks:',
+          '',
+        ].concat(tasks);
+        if (templates.length > 0) {
+          output = output.concat([
+            '',
+            'And the following templates:',
+            '',
+          ]).concat(templates);
+        }
+        output.push(
+          '',
+          '_(Browse the [' + rolebase + '](' + rolebase + ') directory for more information)_'
+        );
+        return output.join('\n');
+      },
+    }),
+  ];
+
+  // Replacements to make after the markdown is parsed into HTML.
   var postReplacements = [
     // Change relative project URLs to absolute
-    [/(<a href=")([^"]+)([^>]+>)([^<]+)/g, function(all, pre, href, post, text) {
+    [/(<a href=")([^"]+)([^>]+>)([^<]+)/g, function(src, all, pre, href, post, text) {
       if (/^(?:https?:\/\/|#)/.test(href)) { return all; }
-      var baseHref = githubUrl + 'blob/master/deploy/';
+      var baseHref = githubUrl + 'blob/' + branch + '/deploy/';
       if (/^\.\.\//.test(href)) {
         // Vagrantfile
         text = text.slice(3);
@@ -17,14 +86,14 @@ module.exports = function(grunt) {
         // The deploy folder itself
         href = '';
         baseHref = baseHref.slice(0, -1);
-      } else {
+      } else if (text === href) {
         // Files within the deploy folder
         text = 'deploy/' + text;
       }
       return pre + baseHref + href + post + text;
     }],
     // Make headers "linkable" in the Github style
-    [/(<h\d id=")([^"]+)([^>]+>)/g, function(_, pre, id, post) {
+    [/(<h\d id=")([^"]+)([^>]+>)/g, function(src, all, pre, id, post) {
       return pre + id + post + '<a class="anchor" aria-hidden="true" href="#' + id + '"><span class="octicon octicon-link"></span></a>';
     }],
     // Footer
@@ -37,7 +106,7 @@ module.exports = function(grunt) {
         ].join(' • ') +
         '</div>';
     }],
-  ]
+  ];
 
   grunt.initConfig({
     clean: {
@@ -61,9 +130,16 @@ module.exports = function(grunt) {
       },
       readme: {
         options: {
+          preCompile: function(src, context) {
+            return preReplacements.reduce(function(src, arr) {
+              // return src.replace.apply(src, arr);
+              return src.replace(arr[0], arr[1].bind(null, src));
+            }, src);
+          },
           postCompile: function(src, context) {
             return postReplacements.reduce(function(src, arr) {
-              return src.replace.apply(src, arr);
+              // return src.replace.apply(src, arr);
+              return src.replace(arr[0], arr[1].bind(null, src));
             }, src);
           },
         },
@@ -86,5 +162,7 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.registerTask('default', ['clean', 'copy', 'markdown']);
+  grunt.registerTask('build', ['clean', 'copy', 'markdown']);
+  grunt.registerTask('dev', ['build', 'watch']);
+  grunt.registerTask('default', ['build']);
 };
